c5e782f7c6249bdb63b8db943c8214eeae5e9ec2
matt <matthynesdev@gmail.com>
2017-04-02 14:36:42 -0230

    Class skeletons for player, dealer, deck, and game

---
+++ blackjack.py
@@ -0,0 +1,18 @@
+# the player class
+class Player:
+    pass
+
+
+# the dealer class, very similar to player
+class Dealer:
+    pass
+
+
+# a class representing a deck, contains functions for dealing cards and determining if an ace is usable
+class Deck:
+    pass
+
+
+# the Game class facilitates the game operations such as dealing cards and determining winners
+class Game:
+    pass
